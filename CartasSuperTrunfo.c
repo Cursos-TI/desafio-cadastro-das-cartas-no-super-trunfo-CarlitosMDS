8c54021e3cd2b769dbc04066ff220b54ce62853d
CarlitosMDS <carloshenriquerbds@gmail.com>
2025-01-13 20:15:33 +0000

    Base do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,8 +17,33 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int codigo, populacao, area, pib, npt;
+    char nome[50];
+    
     printf("Desafio xadrez!\n");
-    printf("Novo Commit\n");
+
+    printf("Nome da cidade:\n");
+    scanf("%s", &nome);
+
+    printf("População:\n");
+    scanf("%d", &populacao);
+
+    printf("Area:\n");
+    scanf("%d", &area);
+
+    printf("PIB:\n");
+    scanf("%d", &pib);
+
+    printf("Numero de pontos turisticos:\n");
+    scanf("%d", &npt);
+
+
+    printf("Nome da Cidade: %s \n", &nome);
+    printf("População: %d \n", &populacao);
+    printf("Area: %d \n", &area);
+    printf("PIB: %d\n", &pib);
+    printf("Numero de pontos turisticos: %d \n", &npt);
+    
 
     return 0;
 }
